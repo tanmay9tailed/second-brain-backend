7326d9d7acf329683901ddd98036e83473c5bda9
Tanmay Sahu <sahutanmay1903@gmail.com>
2024-12-05 17:33:01 +0530

    initial

--- src/index.ts
+++ src/index.ts
@@ -207,6 +207,11 @@ app.get("/api/v1/shared-brain/:hash", async (req: Request, res: Response): Promi
     });
   }
 });
+
+app.get("/", (req, res) => {
+  res.send("Server Started");
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on PORT -> ${process.env.PORT}`);
 });
